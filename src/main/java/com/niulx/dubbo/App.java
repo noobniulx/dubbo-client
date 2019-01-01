fdc385c3ea32f7c6d5698492d4d1f18bf92729c6
niulx <24193373@qq.com>
2019-01-01 16:11:49 +0800

    test

--- src/main/java/com/niulx/dubbo/App.java
+++ src/main/java/com/niulx/dubbo/App.java
@@ -6,6 +6,8 @@ public class App {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("dubbo-client.xml");
         Ihello ihello = (Ihello) context.getBean("helloService");
+        Test test = (Test) context.getBean("test");
         System.out.println(ihello.sayHello("niulx"));
+        System.out.println(test.test("hessian"));
     }
 }
